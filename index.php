a11e6a4821ae852f384ff4ceea83e1f0c9e04ff4
AaronHuntington <8ahuntington@gmail.com>
2017-02-08 11:19:22 -0600

    Add homepage, css, and login. php

--- index.php
+++ index.php
@@ -1,1 +1,44 @@
-This is a test. 
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Registration Page</title>
+        <link rel="stylesheet" href="css/styles.css"  />
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="menu">
+                <a href="index.php">Sign Up</a>
+                <a href="login.php">Login</a>
+            </div>
+            <div id="formDiv">
+                <form method="POST" action="index.php" enctype="multipart/form-data">
+
+                    <label>First Name:</label><br/>
+                    <input type="text" name="fname" class="inputFields" required/><br/><br/>
+
+                    <label>Last Name:</label><br/>
+                    <input type="text" name="lname"  class="inputFields" required/><br/><br/>
+
+                    <label>Email:</label><br/>
+                    <input type="text" name="email"  class="inputFields" required/><br/><br/>
+
+                    <label>Password:</label><br/>
+                    <input type="password" name="password" class="inputFields"  required/><br/><br/>
+
+                    <label>Re-enter Password:</label><br/>
+                    <input type="password" name="passwordConfirm"  class="inputFields" required/><br/><br/>
+
+                    <label>Image:</label><br/>
+                    <input type="file" name="image" id="imageupload"/><br/><br/>
+
+
+                    <input type="checkbox" name="conditions" />
+
+                    <label>I agree with terms and conditions</label><br/><br/>
+
+                    <input type="submit"  class="theButtons"  name="submit" />
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
